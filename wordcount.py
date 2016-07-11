f97cf9af46071b795c02ac92fd52c301531b84a7
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:29:22 -0700

    now not case sensitive

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,12 @@
 # put your code here.
-the_file = open('twain.txt')
+the_file = open('test.txt')
 
 word_counts = {}
 
 for line in the_file:
-    line = line.rstrip()
-    list_per_line = line.split(" ")
+    list_per_line = line.rstrip().lower().split(" ")
     for word in list_per_line:
         word_counts[word] = word_counts.get(word, 0) + 1
 
-for word, count in word_counts.items():
+for word, count in word_counts.iteritems():
     print word, count
