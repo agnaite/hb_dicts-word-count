e84da3562eda67d5836933b56273c2762d96a0dc
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 13:01:26 -0700

    removed punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,24 @@
 # put your code here.
-the_file = open('test.txt')
+def count_words(filename):
+    the_file = open(filename)
 
-word_counts = {}
+    word_counts = {}
 
-for line in the_file:
-    list_per_line = line.rstrip().lower().split(" ")
-    for word in list_per_line:
-        word_counts[word] = word_counts.get(word, 0) + 1
+    for line in the_file:
+        #line = line.rstrip('?')
+        list_per_line = line.rstrip().lower().split(" ")
+        # out = [c for c in list_per_line if c not in ('!','.',':', '?')]
+        # print out
+        for word in list_per_line:
+            word = word.rstrip('?')
+            word = word.rstrip('!')
+            word = word.rstrip('.')
+            word = word.rstrip(',')
+            word_counts[word] = word_counts.get(word, 0) + 1
 
-for word, count in word_counts.iteritems():
-    print word, count
+    the_file.close()
+
+    for word, count in word_counts.iteritems():
+        print word, count
+
+count_words('test.txt')
