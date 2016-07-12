cea55daf28ea4469dab3f60b69683dce19c01868
Agnė Klimaitė <agnaite@me.com>
2016-07-11 19:43:48 -0700

    refactored

--- wordcount.py
+++ wordcount.py
@@ -1,38 +1,35 @@
-# put your code here.
+#!/usr/bin/env python
+
 import sys
+from collections import Counter
 
-def count_words(filename):
-    the_file = open(filename)
 
-    word_counts = {}
+def count_words(handle):
 
-    for line in the_file:
-        #line = line.rstrip('?')
-        list_per_line = line.rstrip().lower().split(" ")
-        # out = [c for c in list_per_line if c not in ('!','.',':', '?')]
-        # print out
-        for word in list_per_line:
-            word = word.rstrip('?')
-            word = word.rstrip('!')
-            word = word.rstrip('.')
-            word = word.rstrip(',')
-            word_counts[word] = word_counts.get(word, 0) + 1
+    word_counts = Counter()
 
-    the_file.close()
+    for line in handle:
 
+        line = line.strip().lower().split(" ")
+        line = [w for w in line if not w == '']
+
+        for word in line:
+
+            word = word.\
+                rstrip("?").\
+                rstrip("!").\
+                rstrip(".").\
+                rstrip(",")
+
+            word_counts[word] += 1
+
+    # reverse-sort word counts by how many items
     word_counts = word_counts.items()
-    
-    count = 0
-    for tpl in word_counts:
-        word_counts[count] = list(tpl)
-        word_counts[count].reverse()
-        count += 1
+    word_counts.sort(key=lambda x: x[1], reverse=True)
 
-    word_counts.sort()
-    word_counts.reverse()
+    for count, word in word_counts[:20]:
+            print "{}: {}".format(word, count)
 
-    for count, word in word_counts:
-        print "{}: {}".format(word, count)
 
-count_words(sys.argv[1])
-
+with open(sys.argv[1]) as handle:
+    count_words(handle)
